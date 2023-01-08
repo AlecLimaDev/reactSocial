e8703aa185df91e6cde2c11be09e3176d88bd307
Alec Lima <aleclimadev@gmail.com>
2023-01-08 13:01:30 -0300

    feat: config de validação de usuário criado

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {register} = require("../controllers/UserController")
 
 const validate = require("../middlewares/handleValidation");
+const {
+    userCreateValidation
+  } = require("../middlewares/userValidations");
 
-
-router.post("/register", validate, register)
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
