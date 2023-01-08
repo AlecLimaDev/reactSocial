ae737295c2ed12cacdbc366612637d76de9d3044
Alec Lima <aleclimadev@gmail.com>
2023-01-08 14:44:13 -0300

    feat: config qu cria e verifica se o user ja está no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,7 +12,39 @@ const generateToken = (id) => {
 };
 
 const register = async(req, res) => {
-    res.end("Registro")
+
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail "] });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+
+
 }
 
 
