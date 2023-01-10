4e25c390a788299468a8e1fa61e41dab6e0ff6c9
Alec Lima <aleclimadev@gmail.com>
2023-01-09 23:44:44 -0300

    feat: controle de usuario, fotos, novas requisições e interação com fotos

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const {
   register,
   login,
@@ -15,9 +16,21 @@ const {
   loginValidation,
   userUpdateValidation,
 } = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
-router.get("/profile", getCurrentUser);
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
+router.get("/:id", getUserById)
+
 
 module.exports = router;
