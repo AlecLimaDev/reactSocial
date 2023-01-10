0a48f11d2a686c4f301a6a248dec46917ca11065
Alec Lima <aleclimadev@gmail.com>
2023-01-09 23:21:40 -0300

    feat: validações de usuario, login e novas api's

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -43,11 +43,100 @@ const register = async(req, res) => {
     _id: newUser._id,
     token: generateToken(newUser._id),
   });
-
-
 }
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+};
+
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
+
+const update = async (req, res) => {
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+    "-password"
+  );
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
+};
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+};
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
+    update,
+    getUserById,
+
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const {register} = require("../controllers/UserController")
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+  getUserById,
+} = require("../controllers/UserController");
 
 const validate = require("../middlewares/handleValidation");
 const {
-    userCreateValidation
-  } = require("../middlewares/userValidations");
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
 
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", getCurrentUser);
 
 module.exports = router;
